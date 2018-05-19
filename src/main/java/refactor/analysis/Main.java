ee70b7b62b93c7254f8e3cc790a908d6dc9bd2f5
yannickzj <yannickzj@gmail.com>
2018-05-18 23:27:49 -0400

    add linux java path

--- src/main/java/refactor/analysis/Main.java
+++ src/main/java/refactor/analysis/Main.java
@@ -13,12 +13,13 @@ public class Main {
     public static void main(String[] args) {
 
         // use java8 as soot cp
-        String sootCP = "inputs/:/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home/jre/lib/rt.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home/jre/lib/jce.jar:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar";
+        //String sootCP = "inputs/:/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home/jre/lib/rt.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home/jre/lib/jce.jar:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar";
+        String sootCP = "inputs/:/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/rt.jar:/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/jce.jar:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar";
 
         // use java7 as soot cp
         //String sootCP = "inputs/:/Library/Java/JavaVirtualMachines/zulu1.7.0_171.jdk/Contents/Home/jre/lib/rt.jar:/Library/Java/JavaVirtualMachines/zulu1.7.0_171.jdk/Contents/Home/jre/lib/jce.jar:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar";
 
-        String[] sootArgs = new String[] {"-cp", sootCP, "-p", "jb", "use-original-names:true", "-f", "jimple", "-w", "refactor.analysis.examples.MainTest"};
+        String[] sootArgs = new String[] {"-cp", sootCP, "-p", "jb", "use-original-names:true", "-f", "jimple", "-w", "-process-dir", "inputs/"};
         PackManager.v().getPack("wjtp").add(new Transform("wjtp.rf", new RefactorTransformer()));
         PackManager.DEBUG = true;
 
